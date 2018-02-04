34a4bc2bfcc6b0518346df3034cef22cd1de1cc5
Charpell <charpellumeh@gmail.com>
2018-02-04 07:39:43 +0100

    Add coverage reports to Jest

--- __tests__/sum.js
+++ __tests__/sum.js
@@ -1,3 +1,10 @@
+const sum = require('../sum')
+
 test('it works', () => {
-  
+  const result = sum(2, 2)
+  // if (result !== 3) {
+  //   throw new Error('not three')
+  // }
+
+  expect(result).toBe(4)
 })
